4dd69cb56eda4e0628327c72a9ad76c61f4d4456
siddharthapalav@gmail.com <siddharthapalav@gmail.com>
2025-12-06 21:36:31 +0530

    working of react checked

---
+++ React_Foundations/3.FirstReactProject/first-app/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Welcome to Reactjs by me")
+  );
+}
+
+export default App;
